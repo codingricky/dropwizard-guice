ef7aef9fe64d421e433333bdf1ad4c0194413511
Ricky <ricky@dius.com.au>
2014-08-04 11:28:54 +1000

    initial add

---
+++ src/main/java/com/github/codingricky/ServerConfiguration.java
@@ -0,0 +1,14 @@
+package com.github.codingricky;
+
+import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class ServerConfiguration extends Configuration {
+
+    @NotEmpty
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+}
